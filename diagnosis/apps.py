2202900c87a844824211b4b9ff821e276864dc25
frontend-ashwamedhgroup <frontend@ashwamedhgroup.com>
2026-08-12 16:18:16 +0530

    Delete diagnosis directory

--- diagnosis/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class DiagnosisConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'diagnosis'
-    verbose_name = 'Plant Disease Diagnosis'
