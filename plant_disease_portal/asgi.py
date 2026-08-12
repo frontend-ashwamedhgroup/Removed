9a365f9e7fd204730bb1e3391f78b8f8c15f1866
frontend-ashwamedhgroup <frontend@ashwamedhgroup.com>
2026-08-12 16:19:41 +0530

    Delete plant_disease_portal directory

--- plant_disease_portal/asgi.py
+++
@@ -1,5 +0,0 @@
-import os
-from django.core.asgi import get_asgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'plant_disease_portal.settings')
-application = get_asgi_application()
